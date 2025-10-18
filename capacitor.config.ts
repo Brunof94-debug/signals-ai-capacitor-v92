dc0e08957fd80f4c68c2240520516bf4400ca12d
Brunof94-debug <brunoprof07@gmail.com>
2025-10-17 20:31:26 -0700

    Add Capacitor config file

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+const config = {
+  appId: 'app.br.signals.ai',
+  appName: 'Signals AI',
+  webDir: 'web',
+  bundledWebRuntime: false,
+  android: {
+    allowMixedContent: true
+  }
+};
+
+export default config;
